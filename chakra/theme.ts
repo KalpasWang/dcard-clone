4a321a6679289e32b912679d3a1b6a8671fa19b4
Kalpas Wang <wf6212@gmail.com>
2022-12-28 14:30:41 +0800

    temp

---
+++ chakra/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  fonts: {
+    body: `Roboto, 'Helvetica Neue', Helvetica, Arial, 'PingFang TC', '黑體-繁', 'Heiti TC', '蘋果儷中黑', 'Apple LiGothic Medium', '微軟正黑體', 'Microsoft JhengHei', sans-serif`,
+  },
+  colors: {
+    brand: {
+      blue: '#006AA6',
+    },
+  },
+  components: {
+    // Button
+  },
+});
