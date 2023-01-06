66bf2e51a39df8af48462568629ee53846bdad9b
KalpasWang <wf6212@gmail.com>
2023-01-06 23:39:36 +0800

    navbar layout complete

--- chakra/theme.ts
+++ chakra/theme.ts
@@ -15,6 +15,6 @@ export const theme = extendTheme({
     },
   },
   components: {
-    Button,
+    // Button,
   },
 });
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    APP_NAME: 'Dcard',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
